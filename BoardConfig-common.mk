2c0fa7a5ec4219375db80cdb8382a3ec8df98146
jrcable1 <jrcable1@gmail.com>
2026-06-01 10:01:04 -0400

    zuma: add support for OrangeFox recovery

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -277,3 +277,5 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
 
 # Protected VM firmware
 BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
+
+BOARD_PREBUILT_VENDOR_BOOTIMAGE := $(wildcard device/google/OrangeFox/Fox.img)
